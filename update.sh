821e1602c16db22cce19bfc4bcc9d7e315db13d2
0ctoPusK <nodeforyou78@gmail.com>
2024-06-04 04:17:02 +0900

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # in order to update the script to the last version without losing your configuration,
-# first go in evernode_monitor folder:
+# first go in Evernode-Monitor-Split folder:
 #
-# cd evernode_monitor
+# cd Evernode-Monitor-Split
 #
 # and then give execute permission to the script:
 #
@@ -18,22 +18,22 @@ echo  "move to parent folder"
 cd ..
 
 echo "backup .env file"
-cp evernode_monitor/.env .env
+cp Evernode-Monitor-Split/.env .env
 
-echo "remove evernode_monitor folder"
-rm evernode_monitor -r -f
+echo "remove Evernode-Monitor-Split folder"
+rm Evernode-Monitor-Split -r -f
 
 echo "clone the github repo"
 git clone https://github.com/genesis-one-seven/evernode_monitor/
 
 echo "restore .env file"
-cp  .env evernode_monitor/.env
+cp  .env Evernode-Monitor-Split/.env
 
 echo "delete backup .env file"
 rm  .env
 
 echo "go back to script folder"
-cd evernode_monitor
+cd Evernode-Monitor-Split
 
 echo "install dependencies"
 npm install
